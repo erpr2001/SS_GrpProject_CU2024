aa424d4dceb38a40faa94653823c3c191791591f
erpr2001 <eprachi2001@gmail.com>
2024-11-13 16:04:51 -0500

    test_encryption key generated randomly and now not hardcoded.

--- tests/test_encryption.py
+++ tests/test_encryption.py
@@ -5,10 +5,11 @@ from src.encryption.rsa import generate_rsa_keys, encrypt_with_rsa, decrypt_with
 
 class TestEncryption(unittest.TestCase):
     def test_aes_encryption(self):
-        key = b"thisisaverysecretkey256bits!!"  # Or let the function generate a random key
+        # key = b"thisisaverysecretkey256bits!!"  
         original_data = b"Hello, this is a test."
+        # Or let the function generate a random key
         key, iv, encrypted_data = encrypt_message_aes(original_data, key)  # Returns key, iv, and ciphertext
-        decrypted_data = decrypt_message_aes(key, iv, encrypted_data)
+        decrypted_data = decrypt_message_aes(key, iv, encrypted_data)   # Decrypt with the same key and iv
         self.assertEqual(decrypted_data, original_data)
 
     def test_rsa_encryption(self):
